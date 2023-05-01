f9cb45ccc872ed611f1acc9432e163a4eac2ea6b
sat2llite <raumschiff_@naver.com>
2023-05-01 18:21:46 +0900

    sub01 js, css
    애플티비, 아케이드 수정

--- js/sub02.js
+++ js/sub02.js
@@ -3,13 +3,12 @@ const inputBox = document.querySelector(".input_box");
 const inputRowBox = document.querySelector(".input_row_box");
 const content02 = document.querySelector("#content02");
 
+
 inputBoxWrap.addEventListener("focusin", () => {
-  inputRowBox.style.maxHeight = "500px";
-  inputRowBox.style.overflow = "visible";
-  inputRowBox.style.display = "block";
+  inputRowBox.classList.add('on');
   content02.style.height = "auto";
 });
 
 inputBoxWrap.addEventListener("focusout", () => {
-  inputRowBox.style.display = "none";
+  inputRowBox.classList.remove('on');
 });
