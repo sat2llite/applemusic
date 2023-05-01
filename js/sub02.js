3f4aad8d8f6fb199d2469ed6e47784de813a1ac2
sat2llite <raumschiff_@naver.com>
2023-05-01 17:00:07 +0900

    sub02 완료
    sub02 반응형, js 완료
    나중에 수정 사항 있으면 다시 추가 예정

--- js/sub02.js
+++ js/sub02.js
@@ -1,12 +1,13 @@
 const inputBoxWrap = document.querySelector(".inputbox_wrap");
 const inputBox = document.querySelector(".input_box");
 const inputRowBox = document.querySelector(".input_row_box");
-// const inputRowWrap = document.querySelector(".input_row_wrap");
+const content02 = document.querySelector("#content02");
 
 inputBoxWrap.addEventListener("focusin", () => {
   inputRowBox.style.maxHeight = "500px";
   inputRowBox.style.overflow = "visible";
   inputRowBox.style.display = "block";
+  content02.style.height = "auto";
 });
 
 inputBoxWrap.addEventListener("focusout", () => {
