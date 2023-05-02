fc69322fabf90505e363c6a0f8e6ecba04952cf5
sat2llite <raumschiff_@naver.com>
2023-05-02 17:24:10 +0900

    sub01, sub02 완료
    sub02 풀페이지 jQuery 적용

--- js/sub02.js
+++ js/sub02.js
@@ -4,12 +4,41 @@ const inputRowWrap = document.querySelector(".input_row_wrap");
 const inputRowBox = document.querySelector(".input_row_box");
 const content02 = document.querySelector("#content02");
 
-
 inputBoxWrap.addEventListener("focusin", () => {
-  inputRowBox.classList.add('on');
+  inputRowBox.classList.add("on");
   content02.style.height = "auto";
 });
 
 inputBoxWrap.addEventListener("focusout", () => {
-  inputRowBox.classList.remove('on');
+  inputRowBox.classList.remove("on");
+  content02.style.height = "100vh";
+});
+
+
+// 풀페이지(jQuery)
+var $html = $("html");
+var page = 1;
+var lastPage = $(".content").length;
+
+$html.animate({ scrollTop: 0 }, 10);
+
+window.addEventListener("wheel", function(e){
+	e.preventDefault();
+},{passive : false});
+
+$(window).on("wheel", function (e) {
+  if ($html.is(":animated")) return;
+
+  if (e.originalEvent.deltaY > 0) {
+    if (page == lastPage) return;
+
+    page++;
+  } else if (e.originalEvent.deltaY < 0) {
+    if (page == 1) return;
+
+    page--;
+  }
+  var posTop = (page - 1) * $(window).height();
+
+  $html.animate({ scrollTop: posTop }, 700);
 });
