1bfbfd9225bfa445ec7b13af60d6ff57570b3053
sat2llite <raumschiff_@naver.com>
2023-05-03 14:15:14 +0900

    main 햄버거 메뉴

--- js/main.js
+++ js/main.js
@@ -106,3 +106,23 @@ userBtn.addEventListener("click", function () {
     }
   }, 15);
 });
+
+
+// 햄버거 메뉴
+const hamburgerMenu = document.querySelector(".m_header_ul");
+const hamburgerIcon = document.querySelector(".hamburger");
+const hamburgerIconOn = document.querySelector(".hamburger.on");
+
+hamburgerIcon.addEventListener("click", () => {
+  hamburgerIcon.classList.toggle("on");
+
+  if (hamburgerIcon.classList.contains("on")) {
+    hamburgerMenu.style.visibility = "visible";
+    hamburgerMenu.style.opacity = "1";
+    gnb.style.visibility = "hidden";
+  } else {
+    hamburgerMenu.style.visibility = "hidden";
+    hamburgerMenu.style.opacity = "0";
+    gnb.style.visibility = "visible";
+  }
+});
