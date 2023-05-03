15167df675957ed4f282e55cfa9dd2873caa893b
sat2llite <raumschiff_@naver.com>
2023-05-03 13:36:23 +0900

    sub02 풀페이지 주석처리

--- js/sub02.js
+++ js/sub02.js
@@ -16,29 +16,29 @@ inputBoxWrap.addEventListener("focusout", () => {
 
 
 // 풀페이지(jQuery)
-var $html = $("html");
-var page = 1;
-var lastPage = $(".content").length;
+// var $html = $("html");
+// var page = 1;
+// var lastPage = $(".content").length;
 
-$html.animate({ scrollTop: 0 }, 10);
+// $html.animate({ scrollTop: 0 }, 10);
 
-window.addEventListener("wheel", function(e){
-	e.preventDefault();
-},{passive : false});
+// window.addEventListener("wheel", function(e){
+// 	e.preventDefault();
+// },{passive : false});
 
-$(window).on("wheel", function (e) {
-  if ($html.is(":animated")) return;
+// $(window).on("wheel", function (e) {
+//   if ($html.is(":animated")) return;
 
-  if (e.originalEvent.deltaY > 0) {
-    if (page == lastPage) return;
+//   if (e.originalEvent.deltaY > 0) {
+//     if (page == lastPage) return;
 
-    page++;
-  } else if (e.originalEvent.deltaY < 0) {
-    if (page == 1) return;
+//     page++;
+//   } else if (e.originalEvent.deltaY < 0) {
+//     if (page == 1) return;
 
-    page--;
-  }
-  var posTop = (page - 1) * $(window).height();
+//     page--;
+//   }
+//   var posTop = (page - 1) * $(window).height();
 
-  $html.animate({ scrollTop: posTop }, 700);
-});
+//   $html.animate({ scrollTop: posTop }, 700);
+// });
